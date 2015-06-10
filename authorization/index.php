ca2fb2a60636467a5e2a548be6a37f9e1ac6296f
Dzyuba vlad <Dzyubavlad@gmail.com>
2015-06-10 19:11:11 +0300

    Initial commit for authorization

---
+++ authorization/index.php
@@ -0,0 +1,16 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Authorization page</title>
+</head>
+<body>
+	<form method="Post">
+		<span>Name:</span><br>
+		<input type="edit" value="Princess"><br>
+		<span>Password:</span><br>
+		<input type="edit" value="my_little_pony"><br>
+		<input type="submit" value="Login"> 
+	</form>
+</body>
+</html>
