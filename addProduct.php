daffe2be6a6e159cc462ccd5703e0544c1e26054
synergyo <vladun18@gmail.com>
2015-06-10 20:35:30 +0300

    addProduct

--- addProduct.php
+++ addProduct.php
@@ -11,15 +11,38 @@ $db = getConnection($config_path);
  */
 function addProduct($db, $name, $price) {
     $query = $db->prepare("INSERT INTO products(name, price)
-      VALUES(:name, :price)");
+		VALUES(:name, :price)");
     $query->execute(array(
         "name" => $name,
-        "price" => $price
-));
+        "price" => $price));
 }
 
-addProduct($db, 'windows', 100);
+//addProduct($db, 'windows', 100);
 
-var_dump(getAllProducts($db));
+//var_dump(getAllProducts($db));
+
+if (isset($_POST['add'])) {
+    addProduct($db, $_POST['name'], $_POST['price']);
+//    header("Location: index.php");
+}
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Add new product</title>
+</head>
+<body>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <p><input type="text" name="name" required placeholder="name"/></p>
+        <p><input type="text" name="price" required placeholder="price"/></p>
+        <p><input type="submit" name="add" value="add" class="btn btn-primary center-block"/></p>
+    </form>
+</body>
+</html>
+
 
 
