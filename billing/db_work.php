758905c2a8b3208ea78d0834ab3a7dea112fac67
IvankoRambo <ivankorambo@gmail.com>
2015-06-11 11:53:23 +0300

    fdsf

---
+++ billing/db_work.php
@@ -0,0 +1,82 @@
+<?php
+
+$config_path = 'config/db.ini';
+
+function getConnection($config_path){
+	$config = parse_ini_file($config_path);
+	
+	try{
+		$db = new PDO("mysql:host={$config['host']};dbname={$config['db_name']}", $config['user'], $config['password']);
+	}
+	catch(PDOException $e){
+		echo "Connection is failed:".$e->getMessage();
+		return;
+	}
+	
+	return $db;
+		
+}
+
+
+function getAllProducts($db){
+	$query = $db->prepare("SELECT * FROM products");
+	$query->execute();
+	
+	return ( $query->fetchAll(PDO::FETCH_ASSOC) );
+}
+
+function getLastProduct($db){
+	$query = $db->prepare("SELECT * FROM products where id=(SELECT MAX(id) FROM products)");
+	$query->execute();
+	
+	return ( $query->fetchAll(PDO::FETCH_ASSOC) );
+}
+
+
+function filterProductsKeys($product_info){
+	$keys = [];
+	for($i = 0; $i<count($product_info); $i++){
+		$keys[] = $product_info[$i]['id'];
+	}
+	
+	return $keys;
+}
+
+
+function insertIntoProducts($db, $name, $price){
+	$query = $db->prepare("INSERT INTO products (name, price) VALUE (:name, :price)");
+	$query->bindParam(":name", $name, PDO::PARAM_STR);
+	$price = (string)$price;
+	$query->bindParam(":price", $price, PDO::PARAM_STR);
+	
+	return ( $query->execute() ) ? true : false;
+}
+
+
+
+/*
+ *function for converting products array in JSON
+ */
+ 
+
+function convertProductsInJSON($db, $products_keys){
+		
+	$products_keys_str = '';
+	
+	for($i = 0; $i<count($products_keys); $i++){
+		if($i == count($products_keys)-1)	$products_keys_str .= $products_keys[$i];
+		else $products_keys_str .= $products_keys[$i].', ';
+	}
+	
+	$query = $db->prepare("SELECT * FROM products where id IN (:prod_str)");
+	//$query->bindParam(":prod_str", $products_keys_str, PDO::PARAM_STR);
+	$query->execute();
+	
+	$JSON_products = array("products" => array());
+	$array_products = $query->fetchAll(PDO::FETCH_ASSOC);
+	
+	for($i = 0; $i<count($array_products); $i++) $JSON_products['products'][] = $array_products[$i];
+	
+	return json_encode($JSON_products);
+	
+}
