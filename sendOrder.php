5a21b9b57b5f6a4dece1f41c81962738d108211c
Dzyuba vlad <Dzyubavlad@gmail.com>
2015-06-16 20:05:56 +0300

    Added example of order sending

--- sendOrder.php
+++ sendOrder.php
@@ -1,10 +1,28 @@
 <pre>
 <?php
 
+// function postOrder($db, $product_id, $product_quantity, $card_name, $sum, $user_id = NULL) {
+
+
+	$params = array(
+		'data' => json_encode(array(
+				'product_id' => 1,
+				'product_quantity' => 4,
+				'card_name' => 'BR25522',
+				'sum' => 100,
+				'user_id' => 5
+				)));
+	$defaults = array(
+		CURLOPT_URL => 'dev.big-exercise/testGetOrders.php',
+		CURLOPT_POST => true,
+		CURLOPT_POSTFIELDS => $params
+		);
+	
 	$ch = curl_init();
 
-	curl_setopt($ch, CURLOPT_URL, 'dev.big-exercise/testGetOrders.php');
-	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt_array($ch, $defaults);
+	// curl_setopt($ch, CURLOPT_URL);
+	// curl_setopt($ch, CURLOPT_HEADER, 0);
 
 	curl_exec($ch);
 
